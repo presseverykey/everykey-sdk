844422f126fe88a3b96363b53ff487ba1001c733
Tim Becker <tim.becker@kuriositaet.de>
2012-09-13 20:37:34 +0200

    typing working

--- examples/nerdpursuit/main.c
+++ examples/nerdpursuit/main.c
@@ -16,26 +16,34 @@
 
 
 void main (void) {
+  volatile int i;
   GPIO_SetDir(LED_PORT, LED_PIN, GPIO_Output);
   GPIO_SetDir(KEY_PORT, KEY_PIN , GPIO_Input);
 
   GPIO_SETPULL(KEY_PORT, KEY_PIN, IOCON_IO_PULL_UP);
   init_keyboard();
+  for (i=500000; i!=0; --i) {}
+  GPIO_WriteOutput(LED_PORT, LED_PIN, true);
   SYSCON_StartSystick(SYSTICK_CNTR);
-    GPIO_WriteOutput(LED_PORT, LED_PIN, false);
 }
 
 void systick(void) {
   static bool     key_state = false;
-  static uint32_t count     = 0x00;
+//  static uint32_t count     = 0x00;
 
   key_state = GPIO_ReadInput(KEY_PORT, KEY_PIN);
 
-  if (0 == (count % 1000)) {
-    type("hello!");
+  //GPIO_WriteOutput(0,7,key_state);
+
+  //if ((count != 0) && (0 == (count % 100))) {
+  if (!key_state) {
+    type("Hello world!");
+    //type("\x17\x17\x17\xff");
   }
 
-    GPIO_WriteOutput(LED_PORT, LED_PIN, key_state);
+   // GPIO_WriteOutput(LED_PORT, LED_PIN, key_state);
     //do_nerd_sm(key_state, count++);
+
+  //count++;
 }
 
--- examples/nerdpursuit/typing.c
+++ examples/nerdpursuit/typing.c
@@ -1,5 +1,8 @@
 #include "typing.h"
+#include "pressanykey/pressanykey.h"
 
+#define LED_PORT 0
+#define LED_PIN  7
 uint8_t inBuffer[8];
 uint8_t outBuffer[8];
 
@@ -7,7 +10,7 @@ uint8_t key;
 uint8_t mod;
 
 uint16_t inReportHandler(uint8_t reportType, uint8_t reportId, uint16_t len) {
-  inBuffer[0] = 0;
+  inBuffer[0] = mod;
   inBuffer[1] = 0;
   inBuffer[2] = key;
   inBuffer[3] = 0;
@@ -18,40 +21,57 @@ uint16_t inReportHandler(uint8_t reportType, uint8_t reportId, uint16_t len) {
   return 8;
 }
 
+void outReportHandler (uint8_t reportType, uint8_t reportId, uint16_t len) {}
+
 uint8_t map (char c) {
 	if (('a' <= c) && (c <= 'z')) {
-		c -= 0x61;   // - ascii (see `man ascii`)
-		c += 0x04;  // + usb   (see Hut1_12v2.pdf usb.org)
-	} else if (('A' <= c) && (c <= 'Y')) {
-		c -= 0x41;
-		c += 0x04; // see above.
+		// c -= 0x61;   // - ascii (see `man ascii`)
+		// c += 0x04;  // + usb   (see Hut1_12v2.pdf usb.org)
+    return c - 0x5D;
+	} else if (('A' <= c) && (c <= 'Z')) {
+		//c -= 0x41;
+		//c += 0x04; // see above.
+    return c - 0x3D;
 	} else if (('0' <= c) && (c <= '9')) {
-		c -= 0x30;
-		c += 0x1E;
+	  //	c -= 0x30;
+	  //	c += 0x1E;
+    return c - 0x12;
 	} else {
-		c = '?';
+    switch (c) {
+      case ' ': return 0x2C;
+      case '\n': return 0x28;
+      // TODO various punctuation and brackets ...
+      default: return 0x2D; // '-'
+    }
 	}
-	return (uint8_t)c;
+	return (uint8_t)0x2D;
 }
+
 uint8_t modifier (char c) {
-	
-	if ('A' <= c <= 'Y') { return 225;}
+  if (('A' <= c) && (c <= 'Z')) {	return 0x02;}
 	return 0;
 }
+void delay (int i) {
+  volatile int j;
+  for (j=0; j!=i; ++j) {}
+}
+
 
 void type(char * mes) {
   char curr;
-  while (curr = *mes++) {
+  while (curr = *mes++, curr !=0) {
     key = map(curr);
 		mod = modifier(curr);
     HIDPushReport(HID_REPORTTYPE_INPUT, 0);
+    delay(50000);
     key = 0;
 		mod = 0;
     HIDPushReport(HID_REPORTTYPE_INPUT, 0);
+    delay(50000);
   }
 }
 
 void init_keyboard () {
-  KeyboardInit(inBuffer, outBuffer, inReportHandler, NULL);
+  KeyboardInit(inBuffer, outBuffer, inReportHandler, outReportHandler);
   USB_SoftConnect();
 }
