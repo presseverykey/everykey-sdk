f8d500ac7204826486a7b8daa7c999a59fb097aa
Matthias <mattikra@gmail.com>
2015-01-15 12:35:09 +0100

    Added IAP example, added jlink temp files to gitignore

---
+++ examples/iap/main.c
@@ -0,0 +1,52 @@
+/* Demonstration of In-App Programming (writing to flash from within an application). Note that using IAP
+requires a specific linker script - the IAP routines use the highest 32 bytes of RAM, so it's not available 
+for the remaining application. */
+
+#include "everykey/everykey.h"
+
+#define LED_PORT 0
+#define LED_PIN 7
+
+#define PERSISTENCE_SECTOR 7
+#define PERSISTENCE_PAGE 112
+
+const uint8_t* template_buffer = FLASH_PAGE_ADDRESS(PERSISTENCE_PAGE);
+const uint8_t magic[] = "This text was programmed using IAP.";
+uint8_t __attribute__((aligned(4))) ram_buffer[FLASH_PAGE_SIZE];
+
+void main(void) {
+
+	EVERY_GPIO_SET_FUNCTION(LED_PORT, LED_PIN, PIO, ADMODE_DIGITAL);
+	every_gpio_set_dir(LED_PORT, LED_PIN, OUTPUT);
+
+	memset(ram_buffer, 0, FLASH_PAGE_SIZE);
+	strcpy(ram_buffer, magic);
+
+	bool programmed = !(memcmp(ram_buffer, template_buffer, FLASH_PAGE_SIZE));
+	every_gpio_write(LED_PORT, LED_PIN, programmed);
+
+	if (!programmed) {	//Not programmed: Write ram to flash.
+		iap_prepare_sector(PERSISTENCE_SECTOR);
+		iap_erase_sector(PERSISTENCE_SECTOR);
+		iap_prepare_sector(PERSISTENCE_SECTOR);
+		iap_write_page(PERSISTENCE_PAGE, ram_buffer);
+	}
+}
+
+// void systick() {
+// 	static uint32_t counter = 0;
+// 	counter++;
+// 	uint8_t bit = counter / 64 % 10;
+// 	uint8_t phase = counter % 64;
+// 	uint8_t duty = 0;
+// 	if (bit < 8) {
+// 		duty = (status & (0x80 >> bit)) ? 40 : 10;
+// 	}
+// 	every_gpio_write(LED_PORT, LED_PIN, phase < duty);
+// }
+
+/* store this in the config page */
+__attribute__ ((section(".config")))
+
+const uint8_t config[] = "This is the text that was written when the device was initially flashed.";
+
